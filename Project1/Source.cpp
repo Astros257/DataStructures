02ab1aad3020e4cfa601334e5b440d0d628b5951
jose alvarez <astros257@gmail.com>
2020-05-06 19:48:36 -0500

    hello

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+
 using namespace std;
+
+class node
+{
+public:
+	int data;
+	node* next;
+};
+///stuff
+bool search(node* s, int val)
+{
+	while (s != NULL)
+	{
+		if (s->data == val)
+			return true;
+		s = s->next;
+	}
+	return false;
+}
+
+void printnode(node* s)
+{
+	while (s != NULL)
+	{
+		cout << s->data << "->";
+		s = s->next;
+	}
+	cout << "Null \n";
+}
 
 int main()
 {
-	cout << "Hello World";
-	//this works x2
-	//
-	//this works
+	node* head = new node();
+	node* second = new node();
+	node* third = new node();
+
+
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	printnode(head);
+
 	return 0;
 }
